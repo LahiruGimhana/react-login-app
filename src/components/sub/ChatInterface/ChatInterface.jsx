a9d2da0ea92ad79fd5ac957416b83bad5d68c619
lahirugimhana95@gmail.com <lahirugimhana95@gmail.com>
2021-05-31 18:19:44 +0530

    chatinterface

---
+++ src/components/sub/ChatInterface/ChatInterface.jsx
@@ -0,0 +1,41 @@
+import {React, useState , useEffect,} from 'react';
+import SendMsg from './SendMsg';
+import Chathistory from './ChatHistory';
+import UserDetails from './UserDetails';
+
+import {useDispatch, useSelector } from 'react-redux';
+
+function ChatInterface() {
+
+
+    const [visibility, setVisibility] = useState(false);
+    const [name, setName] = useState("")
+    // let visibility = false;
+    let dispatch = useDispatch();
+    const form_data = useSelector(state => { return state.visible_list })
+
+    useEffect(() => {
+        // console.log(form_data);
+        // console.log(`props pass data is ${form_data.data.FirstName}`);
+
+        // setImage(form_data.data);
+        setVisibility(form_data.visible);
+        setName(form_data.data.FirstName);
+    }, [form_data.data])
+
+    // visibility = form_data.visible;
+
+    return (
+        <>
+            {!visibility &&
+            <div>
+                <UserDetails/>
+                <Chathistory/>
+                <SendMsg consumerData={form_data}/>
+            </div>
+            }
+        </>
+    )
+}
+
+export default ChatInterface
---
+++ src/components/sub/ChatInterface/SendMsg.jsx
@@ -0,0 +1,56 @@
+import {React, useState, usetextValue} from 'react';
+import send from '../../img/send.png';
+import {sendMessage} from '../../../socket';
+
+function SendMsg(props) {
+    // let visible=false;
+    const [visible, setVisible] = useState(false)
+    // console.log(`props pass data is ${props.consumerData.data.FirstName}`);
+    let consumer_name=props.consumerData.data.FirstName+' '+props.consumerData.data.LastName;
+
+    //take login user name using session
+    let sessionData = sessionStorage.getItem('userLoginSessionData');
+
+    let producer_name= JSON.parse(sessionData).name;
+    // console.log(`session data is ${producer_name}`);
+
+
+
+
+    //send text message
+    const [textValue, setTextValue] = useState({ textMessage: ""});
+    
+    const handleInput = event => {
+        setVisible(true);
+        if(event.target.value==''){
+            setVisible(false);
+        }
+        setTextValue({ textMessage: event.target.value });
+      };
+
+    const sendText=()=>{
+        // alert(textValue.textMessage);
+        sendMessage(producer_name, consumer_name, textValue.textMessage);
+    }
+
+    return (
+        <div>
+            <form>
+                <div class="form-row" style={{ margin:"5px"}}>
+                    <div class="form-group" style={{width:"90%", paddingRight:"10px"}}>
+                      {/* <label for="inputAddress">Address</label> */}
+                      <input type="text" class="form-control" id="inputChat" placeholder="enter msg" onChange={handleInput} />
+                    </div>
+                    {/* <a href={sendText}> */}
+                    <button style={{border:"none"}} onClick={sendText}>
+                        {visible && 
+                        <img src={send} style={{width:"35px", height:"35px"}} />
+                        }
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SendMsg
