56c4327f653acfbd40cfbca1821613a95b8b6888
lahirugimhana95@gmail.com <lahirugimhana95@gmail.com>
2021-06-10 18:23:21 +0530

    immer

--- src/redux/reducers/chatReducer.js
+++ src/redux/reducers/chatReducer.js
@@ -1,69 +1,101 @@
 import {RECEIVE_MSG, SEND_MSG } from '../actions/actionTypes';
+import produce from "immer";
 
-const chatReducer = (state={ }, action) => {
+const INITIAL_STATE = {}
+
+const chatReducer=produce((draft, action)=>{
     switch (action.type) {
         case RECEIVE_MSG :
-            // let x={event: "send", from: "gimhana lahiru", to: "admin admin", content: "hello world"};
-
-            // if (action.message) {
-                if (state[action.message.from]) {
-                    let msg={
-                        direction: "RECEIVED",
-                        message: action.message.content,
-                        from: action.message.from,
-                        to: action.message.to,
-                    }
-                    state={...state, [action.message.from] : [...state[action.message.from], msg]}
-                } else {
-                    let msg={
-                        direction: "RECEIVED",
-                        message: action.message.content,
-                        from: action.message.from,
-                        to: action.message.to,
-                    }
-                    state={...state, [action.message.from] : [msg]}
+            if(draft[action.message.from]){
+                let msg={
+                    direction: "RECEIVED",
+                    message: action.message.content,
+                    from: action.message.from,
+                    to: action.message.to,
                 }
+                    //  [draft[action.message.from]] = msg;    
+            }
+            else{
+                let msg={
+                    direction: "RECEIVED",
+                    message: action.message.content,
+                    from: action.message.from,
+                    to: action.message.to,
+                }
+                    draft={[action.message.from] : [msg]}
+            }
+        }
+    }, INITIAL_STATE)
+            // if (state[action.message.from]) {
+            //     let msg={
+            //         direction: "RECEIVED",
+            //         message: action.message.content,
+            //         from: action.message.from,
+            //         to: action.message.to,
+            //     }
+            //     state={...state, [action.message.from] : [...state[action.message.from], msg]}
+            // } else {
+            //     let msg={
+            //         direction: "RECEIVED",
+            //         message: action.message.content,
+            //         from: action.message.from,
+            //         to: action.message.to,
+            //     }
+            //     state={...state, [action.message.from] : [msg]}
+            // }
+        // return state;
 
-                //style of final object
-                // let users={
-                // 	"gimhana":[{ "fname": "gimhana", "lname": 'lahiru'}, {"fcolor":'blue', "lcolor":'yello'}],
-                // 	"sadun":[{ "fname": 'sadun', "lname": 'hasan'}, {"fcolor":'white', "lcolor":'black'}],
-                // 	"nimal": [{ "fname": 'aaa', "lname": 'bbb'}, {"fcolor":'red', "lcolor":'green'}],
-                // }
-            // } 
-            // state={...state};
-            // console.log(`aa${state}`)
-            return state;
+// const chatReducer = (state={ }, action) => {
+//     switch (action.type) {
+//         case RECEIVE_MSG :
+//                 if (state[action.message.from]) {
+//                     let msg={
+//                         direction: "RECEIVED",
+//                         message: action.message.content,
+//                         from: action.message.from,
+//                         to: action.message.to,
+//                     }
+//                     state={...state, [action.message.from] : [...state[action.message.from], msg]}
+//                 } else {
+//                     let msg={
+//                         direction: "RECEIVED",
+//                         message: action.message.content,
+//                         from: action.message.from,
+//                         to: action.message.to,
+//                     }
+//                     state={...state, [action.message.from] : [msg]}
+//                 }
+//             return state;
 
 
-            case SEND_MSG:
+//             case SEND_MSG:
 
-                    if(state[action.consumer_name]){
-                        let msg={
-                            direction: "SEND",
-                            message: action.message,
-                            from:action.producer_name,
-                            to: action.consumer_name,
-                        }
-                        state={...state, [action.consumer_name] : [...state[action.consumer_name], msg]}
-                        // state={...state, [sender_and_receiver] : [...state[sender_and_receiver], msg]}
-                    } else {
-                        let msg={
-                            direction: "SEND",
-                            message: action.message,
-                            from:action.producer_name,
-                            to: action.consumer_name,
-                        }
-                        state={...state, [action.consumer_name] : [msg]}
-                        // state={...state, [sender_and_receiver] : [msg]}
-                    }
-            return state;   
+//                     if(state[action.consumer_name]){
+//                         let msg={
+//                             direction: "SEND",
+//                             message: action.message,
+//                             from:action.producer_name,
+//                             to: action.consumer_name,
+//                         }
+//                         state={...state, [action.consumer_name] : [...state[action.consumer_name], msg]}
+//                         // state={...state, [sender_and_receiver] : [...state[sender_and_receiver], msg]}
+//                     } else {
+//                         let msg={
+//                             direction: "SEND",
+//                             message: action.message,
+//                             from:action.producer_name,
+//                             to: action.consumer_name,
+//                         }
+//                         state={...state, [action.consumer_name] : [msg]}
+//                         // state={...state, [sender_and_receiver] : [msg]}
+//                     }
+//             return state;   
                
-        default:
-            return state;       
-    }
-    // return state;       
+//         default:
+//             return state;       
+//     }
+//     // return state;       
 
-}
+// }
 
 export default chatReducer
